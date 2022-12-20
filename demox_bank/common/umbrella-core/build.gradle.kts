92624c01828b408b9a117880a2944554238b0314
demox2642 <demox2642@gmail.com>
2022-12-20 15:37:31 +0500

    Create setting umbrella modules

---
+++ demox_bank/common/umbrella-core/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("multiplatform-setup")
+    id("android-setup")
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":common:core"))
+                implementation(project(":common:core-utils"))
+
+                implementation(Dependencies.Kodein.core)
+            }
+        }
+    }
+}
